e4250e5027d173b2e19b8045891c145f40887907
Matthias <vidalmat06@gmail.com>
2021-02-12 13:40:37 +0100

    ajout POST

---
+++ src/Entity/POST.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\POSTRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=POSTRepository::class)
+ */
+class POST
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $content;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $author;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $postedAt;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Subject::class, inversedBy="pOSTs")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $subject;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(string $content): self
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    public function getAuthor(): ?string
+    {
+        return $this->author;
+    }
+
+    public function setAuthor(string $author): self
+    {
+        $this->author = $author;
+
+        return $this;
+    }
+
+    public function getPostedAt(): ?\DateTimeInterface
+    {
+        return $this->postedAt;
+    }
+
+    public function setPostedAt(\DateTimeInterface $postedAt): self
+    {
+        $this->postedAt = $postedAt;
+
+        return $this;
+    }
+
+    public function getSubject(): ?Subject
+    {
+        return $this->subject;
+    }
+
+    public function setSubject(?Subject $subject): self
+    {
+        $this->subject = $subject;
+
+        return $this;
+    }
+}
--- src/Entity/Subject.php
+++ src/Entity/Subject.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\SubjectRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -17,9 +19,49 @@ class Subject
      */
     private $id;
 
+    /**
+     * @ORM\OneToMany(targetEntity=POST::class, mappedBy="subject", orphanRemoval=true)
+     */
+    private $pOSTs;
+
+    public function __construct()
+    {
+        $this->pOSTs = new ArrayCollection();
+    }
+
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    /**
+     * @return Collection|POST[]
+     */
+    public function getPOSTs(): Collection
+    {
+        return $this->pOSTs;
+    }
+
+    public function addPOST(POST $pOST): self
+    {
+        if (!$this->pOSTs->contains($pOST)) {
+            $this->pOSTs[] = $pOST;
+            $pOST->setSubject($this);
+        }
+
+        return $this;
+    }
+
+    public function removePOST(POST $pOST): self
+    {
+        if ($this->pOSTs->removeElement($pOST)) {
+            // set the owning side to null (unless already changed)
+            if ($pOST->getSubject() === $this) {
+                $pOST->setSubject(null);
+            }
+        }
+
+        return $this;
     }
 
 
