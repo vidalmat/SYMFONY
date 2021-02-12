b015f663567cc77d0d07c2dfad5b41f97f9b3c9c
Matthias <vidalmat06@gmail.com>
2021-02-12 15:07:08 +0100

    ajout composer

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -13,6 +13,8 @@ class HomeController extends AbstractController
      */
     public function index(): Response
     {
+
+        
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
         ]);
