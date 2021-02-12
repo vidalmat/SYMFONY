7fa145aafd90fd0dcdee034fba65b1761bb4dca7
Matthias <vidalmat06@gmail.com>
2021-02-12 11:46:44 +0100

    Ajout subject.php

---
+++ src/Entity/Subject.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\SubjectRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=SubjectRepository::class)
+ */
+class Subject
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+
+    
+}
